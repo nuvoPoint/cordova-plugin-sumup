d16c3156871aebedbe46b770719d77cd390b6be4
Lars Støttrup Nielsen <larsstoettrup@gmail.com>
2018-05-31 19:44:00 +0200

    fix: typings

--- www/sumup.d.ts
+++ www/sumup.d.ts
@@ -1,9 +1,7 @@
 declare module 'cordova-plugin-sumup' {
-  export class Sumup {
-    login(success, failure): void;
-    logout(success, failure): void;
-    settings(success, failure): void;
-    prepare(success, failure): void;
-    pay(success: ([transactionCode, cardType]: [string, string]) => void, failure, amount, currencycode, title): void;
-  }
+  export function login(success, failure): void;
+  export function logout(success, failure): void;
+  export function settings(success, failure): void;
+  export function prepare(success, failure): void;
+  export function pay(success: ([transactionCode, cardType]: [string, string]) => void, failure, amount, currencycode, title): void;
 }
