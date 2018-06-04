74796fd52abd0c83c7d9a056e0b586167c24641b
Lars Støttrup Nielsen <larsstoettrup@gmail.com>
2018-06-04 16:34:50 +0200

    fix: less generic name

--- www/sumup.d.ts
+++ www/sumup.d.ts
@@ -1,10 +1,10 @@
-declare type Success = () => void;
-declare type Failure = (err: Error) => void;
+declare type SumUpSuccess = () => void;
+declare type SumUpFailure = (err: Error) => void;
 
 declare module 'cordova-plugin-sumup' {
-  export function login(accessToken?: string, success?: Success, failure?: Failure): void;
-  export function logout(success?: Success, failure?: Failure): void;
-  export function settings(success?: Success, failure?: Failure): void;
-  export function prepare(success?: Success, failure?: Failure): void;
-  export function pay(amount: number, currencycode: string, title: string, success?: ([transactionCode, cardType]: [string, string]) => void, failure?: Failure): void;
+  export function login(accessToken?: string, success?: SumUpSuccess, failure?: SumUpFailure): void;
+  export function logout(success?: SumUpSuccess, failure?: SumUpFailure): void;
+  export function settings(success?: SumUpSuccess, failure?: SumUpFailure): void;
+  export function prepare(success?: SumUpSuccess, failure?: SumUpFailure): void;
+  export function pay(amount: number, currencycode: string, title: string, success?: ([transactionCode, cardType]: [string, string]) => void, failure?: SumUpFailure): void;
 }
